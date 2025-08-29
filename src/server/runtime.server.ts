a7dba81d311c3ce80afba4959dcf4fd164490317
Alejandro Vela <78871584+SatiXXGG@users.noreply.github.com>
2025-08-28 22:36:19 -0600

    docs: add modding runtime

--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -2,6 +2,7 @@ import { Flamework } from "@flamework/core";
 
 Flamework.addPaths("src/server/components");
 Flamework.addPaths("src/server/services");
+Flamework.addPaths("src/server/modding");
 Flamework.addPaths("src/shared/components");
 
 Flamework.ignite();
