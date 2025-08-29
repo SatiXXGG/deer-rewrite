1fa7a3535252ac55b9d665ee8c32678b1e686f85
Alejandro Vela <78871584+SatiXXGG@users.noreply.github.com>
2025-08-28 22:35:05 -0600

    docs: RoleTags

---
+++ src/shared/types/RoleTags.ts
@@ -0,0 +1,10 @@
+export enum Roles {
+	deer = "Deer",
+	hunter = "Hunter",
+	safeHunter = "SafeHunter",
+	wendigo = "Wendigo",
+	playing = "playing",
+	none = "none",
+}
+
+export const RoleArray = [Roles.deer, Roles.hunter, Roles.wendigo, Roles.safeHunter];
