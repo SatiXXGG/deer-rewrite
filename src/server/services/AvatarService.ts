4c0fb25d332c13e1d869792ffb7b59035d561c46
Alejandro Vela <78871584+SatiXXGG@users.noreply.github.com>
2025-08-28 22:33:44 -0600

    feat: add AvatarService

---
+++ src/server/services/AvatarService.ts
@@ -0,0 +1,25 @@
+import { Service, OnStart } from "@flamework/core";
+import { ReplicatedStorage, Workspace } from "@rbxts/services";
+import { DataService } from "./DataService";
+import { ICharacter } from "shared/components/types/Character";
+
+@Service({})
+export class AvatarService implements OnStart {
+	private deerSkins = ReplicatedStorage.skins;
+	constructor(private DataService: DataService) {}
+	onStart() {}
+	changeDeer(player: Player) {
+		const current = this.DataService.getCurrentDeer(player);
+		const playerCharacter = player.Character as ICharacter | undefined;
+		const skin = ReplicatedStorage.skins.deer.FindFirstChild(current) as ICharacter | undefined;
+		if (skin && playerCharacter) {
+			const character = skin.Clone();
+			character.Name = player.Name;
+			character.HumanoidRootPart.CFrame = playerCharacter.HumanoidRootPart.CFrame;
+			character.SetAttribute("isMorph", true);
+			player.Character = character;
+			character.Parent = Workspace;
+			return character;
+		}
+	}
+}
